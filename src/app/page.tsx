0f66181835c66f5390fff7234b46d71426e716b6
Zana Abdi <zana.abdi@hotmail.com>
2026-01-31 18:19:08 +0330

    rearrange: moved map related files so page.tsx can be server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,4 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const Map = dynamic(() => import("../components/Map"), {
-  ssr: false,
-});
+import Map from "@/components/Map";
 
 export default function Page() {
   return (
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,19 +1,15 @@
 "use client";
 
-import { MapContainer, TileLayer } from "react-leaflet";
+import dynamic from "next/dynamic";
+
+const LeafLetProvider = dynamic(() => import("../components/LeafLetProvider"), {
+  ssr: false,
+});
 
 export default function Map() {
   return (
-    <MapContainer
-      center={[51.505, -0.09]}
-      zoom={13}
-      scrollWheelZoom={true}
-      style={{ height: "100vh", width: "100%" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
+    <div>
+      <LeafLetProvider />
+    </div>
   );
 }
