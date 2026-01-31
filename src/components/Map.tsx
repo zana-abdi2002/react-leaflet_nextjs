851c51603fda33a7fd52b3f450e05f73330c860c
Zana Abdi <zana.abdi@hotmail.com>
2026-01-31 13:05:01 +0330

    feat: implement simple leaflet map component

---
+++ src/components/Map.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { MapContainer, TileLayer } from "react-leaflet";
+
+export default function Map() {
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      scrollWheelZoom={true}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
+}
