32403800a862aef8983fe12dd1a694ab1604afe7
Zana Abdi <zana.abdi@hotmail.com>
2026-02-03 22:18:12 +0330

    feat: search bar changes center of map

--- src/components/LeafLetProvider.tsx
+++ src/components/LeafLetProvider.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import { Station } from "@/types";
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import AnimateViewOnClick from "./AnimateViewOnClick";
+import { Map } from "leaflet";
+
+const zoom = 13;
 
 type LeafLetProviderProps = {
   selectedStation: Station | null;
@@ -15,29 +18,51 @@ export default function LeafLetProvider({
   const lat = station?.lat || 50;
   const lng = station?.lng || 50;
 
-  useEffect(() => {
-    console.log(lat);
-  }, [lat]);
+  const [map, setMap] = useState<Map | null>(null);
 
-  return (
-    <MapContainer
-      center={[51.1657, 10.4515]}
-      zoom={5}
-      scrollWheelZoom={true}
-      style={{ height: "100dvh", width: "100%" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <AnimateViewOnClick />
-      <Marker position={[51.1657, 10.4515]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+  // -----------------------------------------------------------
+
+  useEffect(() => {
+    // alert(lat);
+    map?.setView([lat, lng], zoom);
+
+    const handleMove = () => {
+      // You can add logic here if you want to do something on move
+    };
+
+    map?.on("move", handleMove);
+    return () => {
+      map?.off("move", handleMove);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [lat, lng]);
+  // -----------------------------------------------------------
+
+  const displayMap = useMemo(
+    () => (
+      <MapContainer
+        center={[51.1657, 10.4515]}
+        zoom={5}
+        scrollWheelZoom={true}
+        style={{ height: "100dvh", width: "100%" }}
+        ref={setMap}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <AnimateViewOnClick />
+        <Marker position={[51.1657, 10.4515]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    ),
+    [],
   );
+
+  return <div>{displayMap}</div>;
 }
 
 // TODO: https://react-leaflet.js.org/docs/example-layers-control/ ++ React control
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import dynamic from "next/dynamic";
 import SearchBar from "./SearchBar";
 import { Station } from "@/types";
