f42708c89f22a72418db5bc2858bb49eca824730
Zana Abdi <zana.abdi@hotmail.com>
2026-02-04 12:51:15 +0330

    feat: add loading spinner to LeafLetProvider component

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,10 +4,16 @@ import { useState } from "react";
 import dynamic from "next/dynamic";
 import SearchBar from "./SearchBar";
 import { Station } from "@/types";
+import { CircularProgress } from "@mui/material";
 // import LeafLetProvider from "./LeafLetProvider";
 
 const LeafLetProvider = dynamic(() => import("../components/LeafLetProvider"), {
   ssr: false,
+  loading: () => (
+    <div className="h-full min-h-dvh w-full min-w-dvw flex justify-center items-center">
+      <CircularProgress size={100} />
+    </div>
+  ),
 });
 
 export default function Map() {
