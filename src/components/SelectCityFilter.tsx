eaa023885b950984ad93650314348cb85bc50e63
Zana Abdi <zana.abdi@hotmail.com>
2026-02-07 20:31:12 +0330

    feat: add toggle to show/hide city filter with floating action button

--- src/components/SelectCityFilter.tsx
+++ src/components/SelectCityFilter.tsx
@@ -1,6 +1,8 @@
 import useStations from "@/hooks/useStations";
 import { Station } from "@/types";
-import { Autocomplete, TextField } from "@mui/material";
+import { Autocomplete, TextField, Fab, Zoom } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import { useState } from "react";
 
 type SelectCityFilterProps = {
   filteredCities: string[];
@@ -15,6 +17,7 @@ function SelectCityFilter({
   selectedStation,
   setSelectedStation,
 }: SelectCityFilterProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const { stations } = useStations();
   const cityNames = [...new Set(stations.map((s) => s.city))];
 
@@ -29,28 +32,46 @@ function SelectCityFilter({
   };
 
   return (
-    <div className="fixed left-1/2 transform -translate-x-1/2 z-999 bg-white/80 text-black rounded-2xl shadow-lg px-4 py-2 w-100 pointer-events-auto mt-4">
-      <Autocomplete
-        multiple
-        id="tags-standard"
-        options={cityNames}
-        sx={{ width: "100%" }}
-        // getOptionLabel={}
-        defaultValue={[]}
-        value={filteredCities}
-        onChange={(_, value) => {
-          handleChange(value);
-        }}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            variant="standard"
-            label="Filter Cities"
-            placeholder="Cities"
-          />
-        )}
-      />
-    </div>
+    <>
+      <Fab
+        color="primary"
+        onClick={() => setIsOpen((o) => !o)}
+        sx={{ position: "fixed", bottom: 24, right: 24 }}
+      >
+        <AddIcon />
+      </Fab>
+
+      {isOpen && (
+        <div
+          style={{ zIndex: 1400, right: 24, bottom: 96 }}
+          className="fixed bg-white/80 text-black rounded-2xl shadow-lg px-4 py-2 pointer-events-auto min-w-60 max-w-65"
+          onBlur={() => setIsOpen(false)}
+        >
+          <Zoom in={isOpen}>
+            <Autocomplete
+              multiple
+              id="tags-standard"
+              options={cityNames}
+              sx={{ width: "100%" }}
+              // getOptionLabel={}
+              defaultValue={[]}
+              value={filteredCities}
+              onChange={(_, value) => {
+                handleChange(value);
+              }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  variant="standard"
+                  label="Filter Cities"
+                  placeholder="Cities"
+                />
+              )}
+            />
+          </Zoom>
+        </div>
+      )}
+    </>
   );
 }
 
